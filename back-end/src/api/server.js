9dac808c5ce0121aaad0492288bc1e81025b9a00
Fábio Morais de Andrade <106340992+fabiomoraisandrade@users.noreply.github.com>
2025-05-16 19:22:41 +0000

    implementa `socket` em `server.js` do backend

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,8 +1,22 @@
-const app = require("./app");
 require("dotenv").config();
+const http = require("http");
+const socket = require("socket.io");
+const socketListener = require("../socket");
+const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+socketListener(io);
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
 });
