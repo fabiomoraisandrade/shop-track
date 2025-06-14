fc161a82d4461342c00186c16c4753b22dab8921
Fábio Andrade <fmorais@catitech.com.br>
2025-06-14 20:57:41 -0300

    atualiza teste `getProductById.test.js` do `back-end` para criar usuario vendedor e incluir o `sellerId` no teste do produto

--- back-end/src/tests/integration/v1/products/getProductById.test.js
+++ back-end/src/tests/integration/v1/products/getProductById.test.js
@@ -4,6 +4,9 @@ const app = require("../../../../api/app");
 
 describe("Testa GET /api/v1/products/:id", () => {
   let token;
+  let createdSellerUserId;
+  let productId;
+  let response;
   const imagePath = path.resolve(__dirname, "../../../files/test-image.jpg");
 
   beforeAll(async () => {
@@ -12,22 +15,46 @@ describe("Testa GET /api/v1/products/:id", () => {
       .send({ email: "email1@teste.com", password: "123456" });
 
     token = res.body.token;
-  });
 
-  describe("Testa retorno do produto com sucesso", () => {
-    let productId;
-    let response;
+    const createSellerUserResponse = await request(app)
+      .post("/api/v1/users")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        name: "Usuário Teste",
+        email: "usuario.teste@getid.com",
+        password: "teste123",
+        isAdmin: false,
+    });
 
-    beforeAll(async () => {
-      const createProductResponse = await request(app)
+    createdSellerUserId = createSellerUserResponse.body.id;
+
+    const createProductResponse = await request(app)
         .post("/api/v1/products")
         .set("Authorization", `Bearer ${token}`)
         .field("name", "Teste")
         .field("price", "23.70")
+        .field("sellerId", createdSellerUserId)
         .attach("file", imagePath);
 
-      productId = createProductResponse.body.id;
+    productId = createProductResponse.body.id;
+  });
 
+  afterAll(async () => {
+      if (productId) {
+        await request(app)
+          .delete(`/api/v1/products/${productId}`)
+          .set("Authorization", `Bearer ${token}`);
+      }
+
+      if (createdSellerUserId) {
+        await request(app)
+          .delete(`/api/v1/users/${createdSellerUserId}`)
+          .set("Authorization", `Bearer ${token}`);
+      }
+  });
+
+  describe("Testa retorno do produto com sucesso", () => {
+    beforeAll(async () => {
       response = await request(app)
         .get(`/api/v1/products/${productId}`)
         .set("Authorization", `Bearer ${token}`);
@@ -38,21 +65,11 @@ describe("Testa GET /api/v1/products/:id", () => {
     });
 
     it("Retorna o produto com chaves esperadas", () => {
-      expect(response.body).toHaveProperty("id", productId);
-      expect(response.body).toHaveProperty("name", "Teste");
-      expect(response.body).toHaveProperty("price", "15.00");
-      expect(response.body).toHaveProperty(
-        "urlImage",
-        "http://localhost:3001/images/teste.jpg",
-      );
-    });
-
-    afterAll(async () => {
-      if (productId) {
-        await request(app)
-          .delete(`/api/v1/products/${productId}`)
-          .set("Authorization", `Bearer ${token}`);
-      }
+      expect(response.body).toHaveProperty("id");
+      expect(response.body).toHaveProperty("name");
+      expect(response.body).toHaveProperty("price");
+      expect(response.body).toHaveProperty("urlImage");
+      expect(response.body).toHaveProperty("sellerId");
     });
   });
 
