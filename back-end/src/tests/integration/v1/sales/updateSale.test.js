06c61da6925b57781b6c6b561e1464ad84abe6af
Fábio Andrade <fmorais@catitech.com.br>
2025-06-15 00:01:31 -0300

    atualiza teste `updateSale.test.js` do `back-end` para criar usuario vendedor e o produto

--- back-end/src/tests/integration/v1/sales/updateSale.test.js
+++ back-end/src/tests/integration/v1/sales/updateSale.test.js
@@ -1,23 +1,16 @@
+const path = require("path");
 const request = require("supertest");
 const app = require("../../../../api/app");
 
 describe("Testa PUT /api/v1/sales/:id", () => {
-  let response;
+  let token;
+  let createdSellerUserId;
+  let productId;
+  let sale;
   let getSale;
   let createdSaleId;
-  let token;
-
-  const newSale = {
-    sellerId: 11,
-    totalPrice: 30.0,
-    deliveryAddress: "Rua Xablau",
-    deliveryNumber: "237",
-    status: "Pendente",
-    products: [
-      { id: 2, quantity: 2 },
-      { id: 15, quantity: 2 },
-    ],
-  };
+  let response;
+  const imagePath = path.resolve(__dirname, "../../../files/test-image.jpg");
 
   beforeAll(async () => {
     const loginResponse = await request(app)
@@ -26,12 +19,43 @@ describe("Testa PUT /api/v1/sales/:id", () => {
 
     token = loginResponse.body.token;
 
+    const createSellerUserResponse = await request(app)
+      .post("/api/v1/users")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        name: "Usuário Teste",
+        email: "usuario.teste@getid.com",
+        password: "teste123",
+        isAdmin: false,
+    });
+
+    createdSellerUserId = createSellerUserResponse.body.id;
+
+    const createProductResponse = await request(app)
+      .post("/api/v1/products")
+      .set("Authorization", `Bearer ${token}`)
+      .field("name", "Weissbier 1l")
+      .field("price", "23.70")
+      .field("sellerId", createdSellerUserId)
+      .attach("file", imagePath);
+
+    productId = createProductResponse.body.id;
+
+    sale = {
+      deliveryAddress: "Rua Xablau",
+      deliveryNumber: "237",
+      status: "Pendente",
+      products: [
+        { id: productId, quantity: 2 },
+      ],
+    }
+
     const createdResponse = await request(app)
       .post("/api/v1/sales")
-      .send(newSale)
+      .send(sale)
       .set("Authorization", `Bearer ${token}`);
 
-    createdSaleId = createdResponse.body.id;
+    createdSaleId = createdResponse.body[0].id;
 
     response = await request(app)
       .put(`/api/v1/sales/${createdSaleId}`)
@@ -48,9 +72,23 @@ describe("Testa PUT /api/v1/sales/:id", () => {
   });
 
   afterAll(async () => {
-    await request(app)
+    if (createdSaleId) {
+      await request(app)
       .delete(`/api/v1/sales/${createdSaleId}`)
       .set("Authorization", `Bearer ${token}`);
+    }
+
+    if (productId) {
+      await request(app)
+      .delete(`/api/v1/products/${productId}`)
+      .set("Authorization", `Bearer ${token}`);
+    }
+
+    if (createdSellerUserId) {
+      await request(app)
+      .delete(`/api/v1/users/${createdSellerUserId}`)
+      .set("Authorization", `Bearer ${token}`);
+    }
   });
 
   it("Atualiza sale no banco e volta com status 204", () => {
